af486c56a9160f6fa306a6dda36e3688bd276647
Roberto <s19120168@alumnos.itsur.edu.mx>
2024-03-21 13:41:30 -0600

    Agregue el header

---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menú Desplegable</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+
+<header>
+    <h1>Tienda de Postres</h1>
+    <nav>
+        <ul>
+            <li><a href="index.php">Inicio</a></li>
+            <li class="dropdown">
+                <a href="#" class="dropbtn">Categorías</a>
+                <div class="dropdown-content">
+                    <a href="catalogo.php?categoria=pastel">Pasteles</a>
+                    <a href="catalogo.php?categoria=galleta">Galletas</a>
+                    <a href="catalogo.php?categoria=gelatina">Gelatinas</a>
+                    <a href="catalogo.php?categoria=cupcake">Cupcakes</a>
+                    <a href="catalogo.php">Todos los postres</a>
+                </div>
+            </li>
+            <li><a href="conocenos.php">Conocenos</a></li>
+        </ul>
+    </nav>
+</header>
+
+</body>
+</html>
